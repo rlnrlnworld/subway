bbc24f8a9fc63486d9903014ae6c05c480fe9c91
rlnrlnworld <diiidhe@gmail.com>
2025-07-09 16:39:21 +0900

    feat: 지하펄 역 노선 매핑

--- src/components/StationPanel.tsx
+++ src/components/StationPanel.tsx
@@ -1,8 +1,35 @@
+import { useEffect } from "react"
+import subwayData from '@/data/subway_by_line.json' assert { type: "json" }
+
+type StationInfo = {
+  SUBWAY_ID: number
+  STATN_ID: number
+  STATN_NM: string
+}
+
+type SubwayData = {
+  [lineName: string]: StationInfo[]
+}
+
+const typedData = subwayData as SubwayData
+
 type Props = {
   selectedStation: string
   onClose: () => void
 }
 export default function StationPanel({ selectedStation, onClose }: Props) {
+  useEffect(() => {
+    const matchingIds: number[] = []
+
+    for (const line in subwayData) {
+      typedData[line].forEach( station => {
+        if (station.STATN_NM === selectedStation) {
+          matchingIds.push(station.SUBWAY_ID)
+        }
+      })
+    }
+    console.log(`${selectedStation}역의 SUBWAY_ID 목록:`, matchingIds)
+  }, [selectedStation])
   return(
     <section className="absolute pointer-events-auto top-0 right-0 h-screen w-[40%] p-3">
       <div className="w-full h-full bg-white rounded-md text-black">
